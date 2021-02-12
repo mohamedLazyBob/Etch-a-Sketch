240578857d3e015cb7d80c6b9c239022fe6ca03a
mohamedLazyBob <45185441+mohamedLazyBob@users.noreply.github.com>
2021-02-12 23:13:23 +0100

    add themes, works with button clicks and everything need to fix the footer, and some responsivity if it's possible

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const gridContainerLength = 800;
 let cellsNumber = 3; // max of this is 64, more then it, it's not beatifull 
 let cellLength = gridContainerLength / cellsNumber;
+let cellCollor = 'rgb(107,85,164)';
 
 grid.addEventListener('mouseover', e => {
 	if (e.target.className === 'cell') {
 		e.target.style.background = 'rgba(0, 0, 0, 0)';
-		e.target.style.border = 'rgba(0, 0, 0, 0)';
+		// e.target.style.border = 'rgba(0, 0, 0, 0)';
 	}
 });
 
@@ -31,11 +32,13 @@ rangeObject.addEventListener('change', () => {
 	drawGrid();
 });
 
-clrBtn.addEventListener('click', e => {
+function colorCells() {
 	for (let i = 0; i < grid.children.length; i++) {
-		grid.children.item(i).style.background = 'rgb(107,85,164)';
+		grid.children.item(i).style.background = cellCollor;
 	}
-});
+}
+
+clrBtn.addEventListener('click', colorCells);
 
 let btns = document.querySelectorAll('input[type=checkbox]')
 console.log(btns);
@@ -47,6 +50,13 @@ let themeColors = { 'background-1': 'rgb(167,139,193)',
 					'background-4': 'rgb(246,207,176)',
 					'background-5': 'rgb(253,238,199)',
 					'background-6': 'rgb(193,194,196)'};
+
+let cellsColors = { 'background-1': 'rgb(107,85,164)', 
+					'background-2': 'rgb(224,225,226)',
+					'background-3': 'rgb(251,195,163)',
+					'background-4': 'rgb(102,83,76)',
+					'background-5': 'rgb(255,64,77)',
+					'background-6': 'rgb(251,202,179)'};
 
 function uncheckThumbnails(id) {
 	for (let idx = 0; idx < btns.length; idx++) {
@@ -64,7 +74,12 @@ btns.forEach(element => {
 		else
 			path = `./imgs/${e.target.id}.png`;
 		grid.style.backgroundImage = `url(${path})`;
+		// change the theme.
 		document.getElementsByTagName('body')[0].style.backgroundColor = themeColors[e.target.id] ;
+		cellCollor = cellsColors[e.target.id];
+		let root = document.documentElement;
+		root.style.setProperty('--cells-color', cellCollor);
+		colorCells();
 		// console.log(path);
 		uncheckThumbnails(e.target.id);
 	});
